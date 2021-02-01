a8f7d3b53ce0a0b8bf357f8205ffe7c77e15e8bd
Tina Wang <tina@tina.cafe>
2021-02-01 22:40:57 +0100

    🚀 Base commit

---
+++ nuxt.config.js
@@ -0,0 +1,75 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'server',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Miel Music',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    htmlAttrs: {
+      lang: 'en'
+    },
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    "@/assets/css/base.postcss"
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    { src: '@/plugins/vue-material-design-ripple.js', mode: 'client' },
+    { src: '@/plugins/vuex-persistedstate.js' }
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    '@nuxt/http'
+  ],
+
+  http: {
+    baseUrl: process.env.BACKEND_URL
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    html: {
+      minify: {
+        collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true,
+        minifyURLs: true,
+        removeComments: true,
+        removeEmptyElements: true
+      }
+    }
+  },
+
+  // Server config
+  server: {
+    port: process.env.SRV_PORT,
+    host: '0.0.0.0'
+  }
+}
